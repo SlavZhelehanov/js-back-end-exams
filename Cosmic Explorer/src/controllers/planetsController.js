39a155ef793658d5f00a2343b2f06b99b74802fe
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-02-19 13:33:04 +0200

    Add static "Create" page

--- Cosmic Explorer/src/controllers/planetsController.js
+++ Cosmic Explorer/src/controllers/planetsController.js
@@ -7,4 +7,9 @@ planetsController.get("/", (req, res) => {
     return res.render("planet/catalog");
 });
 
+// CREATE
+planetsController.get("/create", (req, res) => {
+    return res.render("planet/create");
+});
+
 export default planetsController;
