8dc82812d5655514b1e7746edf1c96178d191a9b
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-07 14:18:51 +0200

    Add static "Create product" page

--- Glow Alchemy/src/controllers/cosmeticController.js
+++ Glow Alchemy/src/controllers/cosmeticController.js
@@ -3,8 +3,21 @@ import { Router } from "express";
 
 const cosmeticController = Router();
 
+
+// CATALOG
 cosmeticController.get("/", (req, res) => {
     return res.render("cosmetics/catalog");
 });
+
+// CREATE
+cosmeticController.get("/create", (req, res) => {
+    return res.render("cosmetics/create");
+});
+
+// DETAILS
+
+// EDIT
+
+// SEARCH
 
 export default cosmeticController;
