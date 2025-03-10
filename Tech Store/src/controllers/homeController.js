d584e1e5c1a66393a5119241a4ee0f4a807e9f56
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-10 22:42:51 +0200

    Add "About" page

--- Tech Store/src/controllers/homeController.js
+++ Tech Store/src/controllers/homeController.js
@@ -4,4 +4,6 @@ const homeController = Router();
 
 homeController.get("/", (req, res) => { return res.render("home/home"); });
 
+homeController.get("/about", (req, res) => { return res.render("home/about"); });
+
 export default homeController;
