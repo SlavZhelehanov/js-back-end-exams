bc819d7c4c424cc1ca1ec72e5e733b72f888217c
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-11 22:05:57 +0200

    Dynamically display devices on "Home" page

--- Tech Store/src/controllers/homeController.js
+++ Tech Store/src/controllers/homeController.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
 
+import parseErrorMessage from "../util/parseErrorMessage.js";
+import deviceService from "../services/deviceService.js";
+
 const homeController = Router();
 
-homeController.get("/", (req, res) => { return res.render("home/home"); });
+homeController.get("/", async (req, res) => {
+    try {
+        const devices = await deviceService.getLastTreeAddedDevices();
+
+        return res.render("home/home", { devices });
+    } catch (error) {
+        return res.render("home/home", { messages: parseErrorMessage(error) });
+    }
+});
 
 homeController.get("/about", (req, res) => { return res.render("home/about"); });
 
--- Tech Store/src/services/deviceService.js
+++ Tech Store/src/services/deviceService.js
@@ -1,6 +1,9 @@
 import Device from "../models/Device.js";
 
 export default {
+    getLastTreeAddedDevices() {
+        return Device.find({}, "brand model price image").sort({ createdAt: -1 }).limit(3);
+    },
     createDevice(deviceData) {
         for (const key in deviceData) deviceData[key] = deviceData[key].trim();
 
