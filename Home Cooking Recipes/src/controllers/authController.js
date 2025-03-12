90a5dd0867c5e21aa0493049b4a376813fc0ddbb
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-12 16:48:23 +0200

    Register user

--- Home Cooking Recipes/src/controllers/authController.js
+++ Home Cooking Recipes/src/controllers/authController.js
@@ -14,16 +14,16 @@ authController.get("/register", isGuest, (req, res) => {
     return res.render("auth/register", { pageTitle: "Register - " });
 });
 authController.post("/register", isGuest, async (req, res) => {
-    const { name, email, password, rePassword } = req.body;
+    const { username, email, password, rePassword } = req.body;
 
     try {
-        const user = await authService.register({ name, email, password, rePassword });
+        const user = await authService.register({ username, email, password, rePassword });
         const token = await jwt.sign({ id: user.id, username: user.name, email }, SUPER_SECRET, { expiresIn: "2h" });
 
         res.cookie(COOKIE_NAME, token, { httpOnly: true });
-        return res.redirect("/devices");
+        return res.redirect("/");
     } catch (error) {
-        return res.render("auth/register", { name, email, messages: parseErrorMessage(error) });
+        return res.render("auth/register", { pageTitle: "Register - ", username, email, messages: parseErrorMessage(error) });
     }
 });
 
@@ -42,7 +42,7 @@ authController.post("/login", isGuest, async (req, res) => {
         const token = await jwt.sign({ id: user.id, username: user.name, email }, SUPER_SECRET, { expiresIn: "2h" });
 
         res.cookie(COOKIE_NAME, token, { httpOnly: true });
-        return res.redirect("/devices");
+        return res.redirect("/");
     } catch (error) {
         return res.render("auth/login", { email, messages: parseErrorMessage(error) });
     }
