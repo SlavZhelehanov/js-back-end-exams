d6e0493ee502cb5c72b929b7d5139c8ddf386939
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-13 15:42:26 +0200

    Add "Create" page

--- Home Cooking Recipes/src/controllers/recipeController.js
+++ Home Cooking Recipes/src/controllers/recipeController.js
@@ -7,4 +7,9 @@ recipeController.get("/", async (req, res) => {
     return res.render("recipe/catalog", { pageTitle: "Recipe Catalog - " });
 });
 
+// CREATE
+recipeController.get("/create", async (req, res) => {
+    return res.render("recipe/create", { pageTitle: "Create Recipe - " });
+});
+
 export default recipeController;
