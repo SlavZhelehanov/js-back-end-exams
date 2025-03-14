983f94e70ca6abaa4162f60b0e8ac7ebefeb4fc4
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-14 16:41:00 +0200

    Add basic skeleton

---
+++ Magma Haven/src/util/envConstats.js
@@ -0,0 +1,10 @@
+// ------------------------- Change to your preference -------------------------
+
+export const PORT = 3000;
+
+export const COOKIE_NAME = "Authentication";
+
+export const SUPER_SECRET = "f3mZB8iAaw3WaOGpjlPQ4iqmdkrPeVZFVw+dGWDzJ/8LXCRZkp/cQnU6r8Zjyu+g";
+
+const DB_NAME = "powerOfNature";
+export const DB_URI = `mongodb://127.0.0.1:27017/${DB_NAME}`;
