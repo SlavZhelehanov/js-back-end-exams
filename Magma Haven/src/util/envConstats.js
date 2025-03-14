eadaf0e2f5137cadf1b62735bd6914b616809cf7
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-14 17:12:10 +0200

    Register user

--- Magma Haven/src/util/envConstats.js
+++ Magma Haven/src/util/envConstats.js
@@ -6,5 +6,5 @@ export const COOKIE_NAME = "Authentication";
 
 export const SUPER_SECRET = "f3mZB8iAaw3WaOGpjlPQ4iqmdkrPeVZFVw+dGWDzJ/8LXCRZkp/cQnU6r8Zjyu+g";
 
-const DB_NAME = "powerOfNature";
+const DB_NAME = "magmaHeaven";
 export const DB_URI = `mongodb://127.0.0.1:27017/${DB_NAME}`;
