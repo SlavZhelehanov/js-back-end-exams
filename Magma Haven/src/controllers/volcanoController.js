39f8a34813c36db281c9483a344d5e06df792cfb
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-15 16:30:50 +0200

    Add static "Create volcano page"

--- Magma Haven/src/controllers/volcanoController.js
+++ Magma Haven/src/controllers/volcanoController.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 
+import { isUser } from "../middlewares/authMiddleware.js";
+
 const volcanoController = Router();
 
 // CATALOG
 volcanoController.get("/", async (req, res) => {
     return res.render("volcano/catalog");
+});
+
+// CREATE
+volcanoController.get("/create", isUser, (req, res) => {
+    return res.render("volcano/create");
 });
 
 export default volcanoController;
