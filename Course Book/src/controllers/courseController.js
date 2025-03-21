742194b83edd8fc198d2edbe415edf6fe89e5dd4
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-21 12:21:46 +0200

    Add hard-coded "Details page"

--- Course Book/src/controllers/courseController.js
+++ Course Book/src/controllers/courseController.js
@@ -14,4 +14,9 @@ courseController.get("/", async (req, res) => {
     return res.render("course/catalog");
 });
 
+// DETAILS
+courseController.get("/:id/details", async (req, res) => {
+    return res.render("course/details");
+});
+
 export default courseController;
