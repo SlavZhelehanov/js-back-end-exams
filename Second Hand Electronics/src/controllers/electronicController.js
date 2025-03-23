642698ccf197b04f25d676302f6f2e17a9a39944
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-23 16:21:39 +0200

    Add "Create" page

--- Second Hand Electronics/src/controllers/electronicController.js
+++ Second Hand Electronics/src/controllers/electronicController.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 
+import { isUser } from "../middlewares/authMiddleware.js";
+
 const electronicController = Router();
 
 // CATALOG
 electronicController.get("/", async (req, res) => {
     return res.render("electronic/catalog");
+});
+
+// CREATE
+electronicController.get("/create", isUser, (req, res) => {
+    return res.render("electronic/create");
 });
 
 export default electronicController;
