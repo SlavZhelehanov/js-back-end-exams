dfe0d5872d07abe4d0e87524fde0e3a548c3684c
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-25 23:45:01 +0200

    Add "Home" page

--- Wizard Creatures Wiki/src/controllers/homeController.js
+++ Wizard Creatures Wiki/src/controllers/homeController.js
@@ -1,20 +1,11 @@
 import { Router } from "express";
 
 import parseErrorMessage from "../util/parseErrorMessage.js";
-import deviceService from "../services/deviceService.js";
 
 const homeController = Router();
 
 homeController.get("/", async (req, res) => {
-    try {
-        const devices = await deviceService.getLastTreeAddedDevices();
-
-        return res.render("home/home", { devices });
-    } catch (error) {
-        return res.render("home/home", { messages: parseErrorMessage(error) });
-    }
+    return res.render("home/home");
 });
-
-homeController.get("/about", (req, res) => { return res.render("home/about"); });
 
 export default homeController;
