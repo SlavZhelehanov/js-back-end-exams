af453cfeb52c0cf57ee59e20caad753cd2082bb6
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-26 12:12:45 +0200

    Add "Create" page

--- Wizard Creatures Wiki/src/controllers/creatureController.js
+++ Wizard Creatures Wiki/src/controllers/creatureController.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 
+import { isUser } from "../middlewares/authMiddleware.js";
+
 const creatureController = Router();
 
 // CATALOG
@@ -7,5 +9,9 @@ creatureController.get("/", async (req, res) => {
     return res.render("creature/catalog");
 });
 
+// CREATE
+creatureController.get("/create", isUser, (req, res) => {
+    return res.render("creature/create");
+});
 
 export default creatureController;
