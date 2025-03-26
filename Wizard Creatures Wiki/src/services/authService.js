4f55b7aa3a79402857f5c2f4997c296ef0209f6c
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-26 12:00:47 +0200

    Fix register error messaging

--- Wizard Creatures Wiki/src/services/authService.js
+++ Wizard Creatures Wiki/src/services/authService.js
@@ -2,11 +2,16 @@ import User from "../models/User.js";
 
 export default {
     async register(userData) {
+        let messages = [];
+
         for (const key in userData) userData[key] = userData[key].trim();
 
-        if (userData.password != userData.rePassword) throw new Error("Password and repeat password must match!");
+        if (userData.firstName.length === 0) messages.push("First name is required");
+        if (userData.lastName.length === 0) messages.push("Last name is required");
+        if (userData.password != userData.rePassword) messages.push("Password and repeat password must match!");
+        if (0 < messages.length) throw messages;
 
-        const user = await User.findOne({ email: userData.email });
+            const user = await User.findOne({ email: userData.email });
         if (user) throw new Error("This email is already registered!");
 
         return User.create(userData);
@@ -18,11 +23,11 @@ export default {
         if (0 < messages.length) throw messages;
 
         const user = await User.findOne({ email: userData.email });
-        if (!user) throw new Error("Wrong email or password");        
+        if (!user) throw new Error("Wrong email or password");
 
-        const isValidPassword = await user.comparePassword(userData.password);        
+        const isValidPassword = await user.comparePassword(userData.password);
         if (!isValidPassword) throw new Error("Wrong email or password");
-        
+
         return user;
     }
 };
