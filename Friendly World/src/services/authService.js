7952fdc66395d5649fafada9f3ccf84ea3a83ad6
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-04-04 18:14:39 +0300

    Login user

--- Friendly World/src/services/authService.js
+++ Friendly World/src/services/authService.js
@@ -13,16 +13,16 @@ export default {
     },
     async login(userData) {
         let messages = [];
-        if (!userData.username || userData.username.length < 1) messages.push("Username field can't be empty!");
+        if (!userData.email || userData.email.length < 1) messages.push("Email field can't be empty!");
         if (!userData.password || userData.password.length < 1) messages.push("Password field can't be empty!");
         if (0 < messages.length) throw messages;
 
-        const user = await User.findOne({ username: userData.username });
-        if (!user) throw new Error("Wrong username or password");        
+        const user = await User.findOne({ email: userData.email });
+        if (!user) throw new Error("Wrong email or password");        
 
         const isValidPassword = await user.comparePassword(userData.password);
         
-        if (!isValidPassword) throw new Error("Wrong username or password");
+        if (!isValidPassword) throw new Error("Wrong email or password");
         return user;
     }
 };
