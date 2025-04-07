13208458afb128406660a94761c53155230cfe31
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-04-07 20:44:44 +0300

    Display last tree added animals on Home page

--- Friendly World/src/controllers/homeController.js
+++ Friendly World/src/controllers/homeController.js
@@ -1,7 +1,18 @@
 import { Router } from "express";
 
+import parseErrorMessage from "../util/parseErrorMessage.js";
+import animalService from "../services/animalService.js";
+
 const homeController = Router();
 
-homeController.get("/", (req, res) => { return res.render("home/home"); });
+homeController.get("/", async (req, res) => {
+    try {
+        const animals = await animalService.getAllAnimals({}, "image name need").sort({ createdAt: -1 }).limit(3);
+
+        return res.render("home/home", { animals });
+    } catch (error) {
+        return res.render("home/home", { messages: parseErrorMessage(error) });
+    }
+});
 
 export default homeController;
