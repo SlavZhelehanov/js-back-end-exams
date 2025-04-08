35178454be3f749035d64df57cf96b26f09dc331
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-04-08 18:09:57 +0300

    Add basic skeleton

---
+++ Gaming Team/src/util/envConstats.js
@@ -0,0 +1,10 @@
+// ------------------------- Change to your preference -------------------------
+
+export const PORT = 3000;
+
+export const COOKIE_NAME = "Authentication";
+
+export const SUPER_SECRET = "BP9OeFkgxSgevpuFW7CaFj2ls9Ggl8/IL9V/tkGmgyk3MUVgxKc7mLV5ZUrj/StC";
+
+const DB_NAME = "powerOfNature";
+export const DB_URI = `mongodb://127.0.0.1:27017/${DB_NAME}`;
