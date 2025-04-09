d633b557f416b5c59c6552d29becce0d7172c2c4
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-04-09 17:58:10 +0300

    Register user

--- Gaming Team/src/util/envConstats.js
+++ Gaming Team/src/util/envConstats.js
@@ -4,7 +4,10 @@ export const PORT = 3000;
 
 export const COOKIE_NAME = "Authentication";
 
-export const SUPER_SECRET = "BP9OeFkgxSgevpuFW7CaFj2ls9Ggl8/IL9V/tkGmgyk3MUVgxKc7mLV5ZUrj/StC";
+export const SUPER_SECRET = "5zal2QY/8phv3PzVxkBHwBs8RWogDFBtl9k1jkNq2NNmMlu4bMmXreNA";
 
-const DB_NAME = "powerOfNature";
+const DB_NAME = "gamingTeam";
 export const DB_URI = `mongodb://127.0.0.1:27017/${DB_NAME}`;
+
+// import { randomBytes } from "crypto";
+// randomBytes(42, (err, buff) => console.log(buff.toString("base64")));
