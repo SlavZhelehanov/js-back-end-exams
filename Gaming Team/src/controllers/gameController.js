5b81cde52add63c31cb19e58bca3d1bcca594844
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-04-10 18:42:44 +0300

    Add game "Create" page

--- Gaming Team/src/controllers/gameController.js
+++ Gaming Team/src/controllers/gameController.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 // import { parseErrorMessage } from "../util/parseErrorMessage.js";
-// import { isUser } from "../middlewares/authMiddleware.js";
+import { isUser } from "../middlewares/authMiddleware.js";
 // import gameService from "../services/gameService.js";
 // import { isValidId } from "../middlewares/utlParamsMiddleware.js";
 
@@ -10,6 +10,11 @@ const gameController = Router();
 // CATALOG
 gameController.get("/", async (req, res) => {
     return res.render("game/catalog");
+});
+
+// CREATE
+gameController.get("/create", isUser, (req, res) => {
+    return res.render("game/create");
 });
 
 export default gameController;
