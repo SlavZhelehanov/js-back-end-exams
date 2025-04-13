ea21f41bf7a46034238526dc88b1f2be3febfc02
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-04-13 21:19:20 +0300

    Add "Register" page

--- Book Talk/src/routes.js
+++ Book Talk/src/routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 
 import homeController from "./controllers/homeController.js";
-// import authController from "./controllers/authController.js";
+import authController from "./controllers/authController.js";
 
 const routes = Router();
 
 routes.use("/", homeController);
 
-// routes.use("/auth", authController);
+routes.use("/auth", authController);
 
 routes.all("*", (req, res) => { return res.render("404"); });
 
