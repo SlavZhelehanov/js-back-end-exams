5005c52ce91a6b33bb547f3c1303384452bac58f
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-04-27 15:52:35 +0300

    Add static "Create offer" page

--- Crypto Trade/src/controllers/cryptoController.js
+++ Crypto Trade/src/controllers/cryptoController.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 // import { parseErrorMessage } from "../util/parseErrorMessage.js";
-// import { isUser } from "../middlewares/authMiddleware.js";
+import { isUser } from "../middlewares/authMiddleware.js";
 // import cryptoService from "../services/cryptoService.js";
 // import { getTypeOfCrypto } from "../util/getTypeOfCrypto.js";
 // import { validateQuery } from "../util/validateUrls.js";
@@ -12,6 +12,12 @@ const cryptoController = Router();
 // CATALOG
 cryptoController.get("/", async (req, res) => {
     return res.render("crypto/catalog");
+});
+
+
+// CREATE
+cryptoController.get("/create", isUser, (req, res) => {
+    return res.render("crypto/create");
 });
 
 export default cryptoController;
