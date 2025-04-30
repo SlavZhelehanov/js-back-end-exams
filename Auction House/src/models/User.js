ebd2144647da2f0a0a424b3e35a572c59915c2fa
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-04-30 15:56:38 +0300

    Register user

--- Auction House/src/models/User.js
+++ Auction House/src/models/User.js
@@ -2,16 +2,27 @@ import { Schema, model } from "mongoose";
 import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
-    name: {
+    firstName: {
         type: String,
-        required: [true, "The name's field can't be empty"],
-        minLength: [2, "The name should be between 2 and 20 characters long"],
-        maxLength: [20, "The name should be between 2 and 20 characters long"]
+        required: [true, "The first name's field can't be empty"],
+        minLength: [1, "The First name should be at leas 1 character long"]
+    },
+    lastName: {
+        type: String,
+        required: [true, "The last name's field can't be empty"],
+        minLength: [1, "The Last name should be at least 1 character long"]
     },
     email: {
         type: String,
         required: [true, "The email's field can't be empty"],
-        minLength: [10, "The email should be at least 10 characters long"]
+        validate: {
+            validator: function (value) {
+                // Regex: only letters allowed in name, domain, and extension
+                return /^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+$/.test(value);
+            },
+            // message: props => `${props.value} is not a valid email format.`
+            message: props => `A valid email format is <name>@<domain>.<extension>`
+        }
     },
     password: {
         type: String,
@@ -21,7 +32,7 @@ const userSchema = new Schema({
 
 userSchema.pre("save", async function () {
     if (this.password.length === 0) throw ["The password's field can't be empty"];
-    if (this.password.length < 4) throw ["The password should be at least 4 characters long"];
+    if (this.password.length < 5) throw ["The password should be at least 5 characters long"];
 
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
