08e8e254bc029bdee8553505430f9bd4157d0468
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-05-01 21:38:26 +0300

    Add hard-coded "Browse" page

--- Auction House/src/controllers/auctionController.js
+++ Auction House/src/controllers/auctionController.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { isUser } from "../middlewares/authMiddleware.js";
-// import deviceService from "../services/deviceService.js";
+// import auctionService from "../services/auctionService.js";
 // import parseErrorMessage from "../util/parseErrorMessage.js";
 // import { isValidId } from "../middlewares/verifyIsValidObjectId.js";
 
@@ -10,6 +10,11 @@ const auctionController = Router();
 // CREATE
 auctionController.get("/publish", isUser, (req, res) => {
     return res.render("auction/create");
+});
+
+// CATALOG
+auctionController.get("/", async (req, res) => {
+    return res.render("auction/browse");
 });
 
 export default auctionController;
