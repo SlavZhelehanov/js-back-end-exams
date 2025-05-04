aa0af0274ff43b4f8ddf72c700acf8b244c2db5a
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-05-04 23:27:14 +0300

    Add given resources

--- Job Ads/src/util/envConstats.js
+++ Job Ads/src/util/envConstats.js
@@ -9,5 +9,5 @@ export const SUPER_SECRET = "U7zv82fxPOAPXxhD0y8AVHmP/BkTwuteRdHUg31dSzfZY/ttLmz
 const DB_NAME = "jobAds";
 export const DB_URI = `mongodb://127.0.0.1:27017/${DB_NAME}`;
 
-import {randomBytes} from "crypto";
-randomBytes(48, (err, buff) => console.log(buff.toString("base64")));
+// import {randomBytes} from "crypto";
+// randomBytes(48, (err, buff) => console.log(buff.toString("base64")));
