766b0ee6d40d946ded2284150d5d48ed91281e99
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-05-09 20:01:20 +0300

    Add basic skeleton

---
+++ Real Estate Agency/src/util/envConstats.js
@@ -0,0 +1,13 @@
+// ------------------------- Change to your preference -------------------------
+
+export const PORT = 3000;
+
+export const COOKIE_NAME = "Authentication";
+
+export const SUPER_SECRET = "9L4GuJNiutjNVutw/aPeZJ048hK8n7jiwkX3a2nI5q7kn15ynQaTkd9Df89x2e/6wRg3ROCvdX9zIhQGC/UuWg==";
+
+const DB_NAME = "realEstateAgency";
+export const DB_URI = `mongodb://127.0.0.1:27017/${DB_NAME}`;
+
+// import { randomBytes } from "crypto";
+// randomBytes(64, (err, buff) => console.log(buff.toString("base64")));
