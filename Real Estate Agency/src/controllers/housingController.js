55d5e017b084aa43801660702c25350e0cf0ca7b
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-05-12 17:01:15 +0300

    Create offer

--- Real Estate Agency/src/controllers/housingController.js
+++ Real Estate Agency/src/controllers/housingController.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 import { parseErrorMessage } from "../util/parseErrorMessage.js";
 import { isUser } from "../middlewares/authMiddleware.js";
 import housingService from "../services/housingService.js";
-// import { getTypeOfhousings } from "../util/getTypeOfhousings.js";
+// import { gettypes } from "../util/gettypes.js";
 // import { validateQuery } from "../util/validateUrls.js";
 // import { isValidId } from "../middlewares/utlParamsMiddleware.js";
 
@@ -28,6 +28,16 @@ housingController.get("/:id/details", async (req, res) => {
 // CREATE
 housingController.get("/create", isUser, (req, res) => {
     return res.render("housing/create");
+});
+housingController.post("/create", isUser, async (req, res) => {
+    const { name, type, year, city, image, description, pieces } = req.body;
+
+    try {
+        await housingService.createHousing({ name, type, year, city, image, description, pieces, owner: req.user.id });
+        return res.redirect("/housings");
+    } catch (error) {
+        return res.render("housing/create", { messages: parseErrorMessage(error), type, name, year, city, image, description, pieces });
+    }
 });
 
 // EDIT
--- Real Estate Agency/src/services/housingService.js
+++ Real Estate Agency/src/services/housingService.js
@@ -3,5 +3,9 @@ import Housing from "../models/Housing.js";
 export default {
     getAllHousings() {
         return Housing.find({}, "name description image location typeOfHousing");
+    },
+    createHousing(housingData) {
+        for (const key in housingData) housingData[key] = housingData[key].trim();
+        return Housing.create(housingData);
     }
 };
