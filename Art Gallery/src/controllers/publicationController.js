85a9e795b9aea47896e550d10c92e6967ecd81db
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-05-20 19:10:29 +0300

    Add hard-coded "Gallery page"

---
+++ Art Gallery/src/controllers/publicationController.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+// import parseErrorMessage from "../util/parseErrorMessage.js";
+// import { isUser } from "../middlewares/authMiddleware.js";
+// import publicationService from "../services/publicationService.js";
+// import { isValidId } from "../middlewares/verifyIsValidObjectId.js";
+
+const publicationController = Router();
+
+// GALLERY
+publicationController.get("/", async (req, res) => {
+    return res.render("publication/gallery");
+});
+
+
+
+export default publicationController;
--- Art Gallery/src/routes.js
+++ Art Gallery/src/routes.js
@@ -2,12 +2,15 @@ import { Router } from "express";
 
 import homeController from "./controllers/homeController.js";
 import authController from "./controllers/authController.js";
+import publicationController from "./controllers/publicationController.js";
 
 const routes = Router();
 
 routes.use("/", homeController);
 
 routes.use("/auth", authController);
+
+routes.use("/publications", publicationController);
 
 routes.all("*", (req, res) => { return res.render("404"); });
 
