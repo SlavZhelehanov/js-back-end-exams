c26adc3c91bc05ebeb0f4689626979af64c6cf0c
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-05-24 22:32:24 +0300

    Create publication

--- Art Gallery/src/controllers/publicationController.js
+++ Art Gallery/src/controllers/publicationController.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 
-// import parseErrorMessage from "../util/parseErrorMessage.js";
+import parseErrorMessage from "../util/parseErrorMessage.js";
 import { isUser } from "../middlewares/authMiddleware.js";
-// import publicationService from "../services/publicationService.js";
+import publicationService from "../services/publicationService.js";
 // import { isValidId } from "../middlewares/verifyIsValidObjectId.js";
 
 const publicationController = Router();
@@ -20,6 +20,16 @@ publicationController.get("/:id/details", async (req, res) => {
 // CREATE
 publicationController.get("/create", isUser, (req, res) => {
     return res.render("publication/create");
+});
+publicationController.post("/create", isUser, async (req, res) => {
+    const { title, paintingTechnique, picture, certificate } = req.body;
+
+    try {
+        await publicationService.createPublication({ title, paintingTechnique, picture, certificate, author: req.user.id });
+        return res.redirect("/publications");
+    } catch (error) {
+        return res.render("publication/create", { messages: parseErrorMessage(error), title, paintingTechnique, picture, certificate });
+    }
 });
 
 // EDIT
--- Art Gallery/src/services/publicationService.js
+++ Art Gallery/src/services/publicationService.js
@@ -3,5 +3,10 @@ import Publication from "../models/Publication.js";
 export default {
     getAllPublications() {
         return Publication.find({}, "title usersShared");
-    }
+    },
+    createPublication(publicationData) {
+        for (const key in publicationData) publicationData[key] = publicationData[key].trim();
+
+        return Publication.create(publicationData);
+    },
 };
