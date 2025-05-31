e6b24d977da98996974fb4ce8ced74624d276acd
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-05-31 18:18:55 +0300

    Add "Home" page

--- Wildlife Photography/src/controllers/homeController.js
+++ Wildlife Photography/src/controllers/homeController.js
@@ -1,18 +1,11 @@
 import { Router } from "express";
 
 import parseErrorMessage from "../util/parseErrorMessage.js";
-import publicationService from "../services/publicationService.js";
 
 const homeController = Router();
 
-homeController.get("/", async (req, res) => {
-    try {
-        const publications = await publicationService.getAllPublications();
-
-        return res.render("home/home", { publications });
-    } catch (error) {
-        return res.render("home/home", { messages: parseErrorMessage(error) });
-    }
+homeController.get("/", (req, res) => {
+    return res.render("home/home");
 });
 
 export default homeController;
