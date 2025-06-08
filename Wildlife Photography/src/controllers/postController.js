13afbb4c387e0e9c871e8312637bebbdc1a49cc9
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-06-08 13:36:17 +0300

    Add hard-coded "All Posts / Catalog" page

---
+++ Wildlife Photography/src/controllers/postController.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+// import parseErrorMessage from "../util/parseErrorMessage.js";
+// import { isUser } from "../middlewares/authMiddleware.js";
+// import postService from "../services/postService.js";
+// import { getTypeOfpost } from "../util/getTypeOfpost.js";
+// import { validateQuery } from "../util/validateUrls.js";
+// import { isValidId } from "../middlewares/verifyIsValidObjectId.js";
+
+const postController = Router();
+
+// ALL POSTS
+postController.get("/", async (req, res) => {
+    return res.render("post/all-posts");
+});
+
+export default postController;
--- Wildlife Photography/src/routes.js
+++ Wildlife Photography/src/routes.js
@@ -2,12 +2,15 @@ import { Router } from "express";
 
 import homeController from "./controllers/homeController.js";
 import authController from "./controllers/authController.js";
+import postController from "./controllers/postController.js";
 
 const routes = Router();
 
 routes.use("/", homeController);
 
 routes.use("/auth", authController);
+
+routes.use("/posts", postController);
 
 routes.all("*", (req, res) => { return res.render("404"); });
 
